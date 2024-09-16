ef9a8a906aee16c5f15a43854f6d84892335ca04
crucialabhi <abhishekmishra445577@gmail.com>
2024-09-16 16:25:20 +0530

    varables & datatypes done

---
+++ 01Basics/01_variable.js
@@ -0,0 +1,17 @@
+ const accountId = 123456
+ let accountEmail = "abhishek@google.com"
+ var accountPassword ="12345"
+ accountCity = "Ahmedabad";
+
+ accountEmail="Abc@gmail.com";
+ accountPassword="1212"
+ accountCity ="bhavnagar"
+
+//  accountId = 45678; not allowed we can't change the value of constant variable;
+
+/*
+ prefer not to use var
+ because of issue in block scope and functional scope
+ */
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
+ 
---
+++ 01Basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all js code as newer version
+/* agar nhi likhte he to bhi wo as per the new version hi treat krega */
+
+//alert (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readeability should be high
+
+console.log("Abhishek");
+
+let name ="Abhishek"
+let age  = 10;
+let isLogged = false;
+let state = null;
+
+
+
+// DATA TYPES
+
+//Number => 2 to the power  53
+//Bigint for bigger integer value
+//string => ""
+//boolean => true /false
+//null => standalone value
+//undefined => undefined
+//symbol => unique
+
+//object   //we will learn it further in deep
+
+console.log(typeof "Abhishek");
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
+
+console.table([typeof state, typeof isLogged,typeof age, typeof name])
